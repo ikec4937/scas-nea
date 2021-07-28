3236929f245d8c0382bcbe292febdeb6f75fd35e
Ikechukwu Chidoziem-Onyeanuna <15ikecc@gmail.com>
2021-07-28 18:56:02 +0100

    Temp global login variable, new login file

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def index():
-    return render_template("index.html", title="Home")
+    return render_template("index.html", title="Home", loggedin=False)
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html", title="Login")
 
 if __name__ == "__main__":
     app.run(debug = True)
