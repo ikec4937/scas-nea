3141c910e0e7921060044742ea490adce7ca8613
Ikechukwu Chidoziem-Onyeanuna <15ikecc@gmail.com>
2021-09-01 15:55:28 +0100

    Search and About page made

--- website/main_views.py
+++ website/main_views.py
@@ -5,3 +5,11 @@ main = Blueprint("main_views", __name__)
 @main.route("/")
 def index():
     return render_template("index.html")
+
+@main.route("/search")
+def search():
+    return "<h1>Search<h1>"
+
+@main.route("/about")
+def about():
+    return "<h1>About<h1>"
