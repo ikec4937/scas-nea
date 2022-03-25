57d08d197fba7f3abf8926b9fa127dacca7adcd3
ikec4937 <82965015+ikec4937@users.noreply.github.com>
2022-03-25 11:54:54 +0000

    Updated gitignore; new Javascript to change grades

--- website/static/grade_input.js
+++ website/static/grade_input.js
@@ -1,1 +1,41 @@
-console.log(fetch("./grade_variation.json"));
+function getSelectBoardName() {
+    let selectValue = document.getElementById("boardname").value;
+}
+
+function getSelectCourseType() {
+    let selectValue = document.getElementById("coursetype").value;
+    changeGradeList(coursetype);
+}
+
+function changeGradeList(coursetype) {
+    let html = "";
+    html += "<option value=\"not_selected\" selected>Select a grade</option>"
+    html += "<option value=\"pending\" >Pending</option>"
+    
+    switch(coursetype){
+        case "GCSE":
+            let gcseGrades = ["U", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+            
+            for(let i = 0; i < gcseGrades.length; i++){
+                html += "<option value=\""+gcseGrades[i]+"\">"+gcseGrades[i]+"</option>"
+                //<option value="pending" >Pending</option>
+            }
+
+        break;
+
+        case "BTEC":
+            let btecGrades = ["Merit", "Distinction", "Pass"];
+
+            for(let i = 0; i < btecGrades.length; i++){
+                html += "<option value=\""+gcseGrades[i]+"\">"+btecGrades[i]+"</option>"
+                //<option value="pending" >Pending</option>
+            }
+        
+        break;
+
+        case "CamNat":
+            
+
+    }
+
+}
